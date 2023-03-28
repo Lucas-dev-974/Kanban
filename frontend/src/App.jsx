481ade021921e222e80e3847889ef0bf63430bec
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-28 14:04:22 +0200

    Integration des composant à la page acceuil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createResource, createSignal, onMount } from 'solid-js';
+import { request } from './reqeuest';
+import AddTask from './components/addTask';
+import AddColumn from './components/addColumn';
 
 function App() {
+  var [kanban, setKbn] = createSignal(null)
+
+  onMount(async () => {
+    const cols = await (await request('api/column/all', 'GET', null)).json()
+    const kbn = new jKanban({ 
+      element: '#kanban', 
+      boards:  cols,
+
+      dragItems:  true,
+      dragBoards: true,
+
+      itemAddOptions: {
+        enabled: true,                                              // add a button to board for easy item creation
+        content:  '+' ,   
+        class: 'kanban-title-button btn btn-default btn-xs',         // default class of the button
+        footer: false                                                // position the button on footer
+      }
+    })
+
+    setKbn(kbn)
+  })
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <main class='container mx-auto'>
+      <nav class='flex w-full justify-center py-5'>
+        <p class='text-2xl	font-semibold'>Kanban</p>
+      </nav>
+
+      <section>
+        <AddTask  kbn={kanban()} />
+        <div className="flex">
+          <div id='kanban' class="my-5"></div>
+          <AddColumn kbn={kanban()} />
+        </div>
+        
+
+      </section>
+    </main>
   );
 }
 

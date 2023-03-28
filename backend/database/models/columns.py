f2e6b41b7ca154193b9d07731e1b310b74e1a4f7
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-28 14:01:29 +0200

    Ajout des entités columns & tasks

---
+++ backend/database/models/columns.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, Mapped
+from .tasks import Tasks
+
+try:
+    from databases import Base
+except:
+    from ..databases import Base
+    
+class Columns(Base):
+    __tablename__ = 'columns'
+
+    id       = Column(Integer, primary_key=True) 
+    title    = Column(String)
+    position = Column(String)
+
+    tasks   = relationship(Tasks, backref='columns')
---
+++ backend/database/models/tasks.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+try:
+    from databases import Base
+except:
+    from ..databases import Base
+    
+class Tasks(Base):
+    __tablename__ = 'tasks'
+
+    id       = Column(Integer, primary_key=True) 
+    title    = Column(String)
+    position = Column(Integer)
+    desc     = Column(String)
+
+    column = mapped_column(ForeignKey('columns.id'))

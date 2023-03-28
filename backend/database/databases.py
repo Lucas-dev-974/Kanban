a2e8468a928a312d2bdb347bfd645005216ddb96
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-28 14:01:10 +0200

    fichier renomer

--- backend/database/databases.py
+++ backend/database/databases.py
@@ -1,5 +1,6 @@
 from sqlalchemy     import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 try:
     from getConf import getConfig
@@ -11,3 +12,5 @@ except:
 engine  = create_engine('postgresql://' + getConfig('user') + ':' + getConfig('password') + '@localhost/' + getConfig('database') + '', echo=True)
 session = sessionmaker(bind=engine)
 session = session()
+
+Base = declarative_base()

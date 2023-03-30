89afe9c18634efe8af09050b4e08a1d05ba852ad
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-30 09:40:05 +0200

    MAJ controller position des tache & ajout d'un controller MAJ tache + supression

--- backend/controllers/task.py
+++ backend/controllers/task.py
@@ -7,7 +7,7 @@ from utils              import toJson
 from database.models.columns import Columns
 from database.models.tasks   import Tasks
 
-@hug.post('/create')
+@hug.post('')
 def createTask(body):
     print(body)
     title  = body.get('title')
@@ -17,53 +17,70 @@ def createTask(body):
         return 'Veuillez rensaigner le champ titre !'
 
     column = session.query(Columns).filter_by(id = col_id).first()
-    print('column: ', column)
-
-    task = Tasks(title = title, column=column.id)
+    tasks = session.query(Tasks).filter_by(column = column.id).count()
+    task = Tasks(title = title, column=column.id, position = tasks + 1)
 
     session.add(task)
     session.commit()
 
     return toJson(task, Tasks, True)
 
-@hug.post('/update-position')
-def updatePosition(body):
-    task_id      = body.get('task_id')
-    col_id       = body.get('col_id')
-    from_pos = int(body.get('from_pos'))
-    to_pos   = int(body.get('to_pos'))
-
+@hug.patch('/position')
+def updatePosition(task_id, col_id, from_pos, to_pos):
+    task_id  = int(task_id)
+    col_id   = int(col_id)
+    from_pos = int(from_pos)
+    to_pos   = int(to_pos)
     
-    task  = session.query(Tasks).filter_by(id = task_id).first()  
 
+    task  = session.query(Tasks).filter_by(id = task_id).first()  
+    
     # Pour les 2 premières conditions
     tsk   = session.query(Tasks).filter_by(position = to_pos).first()
-
+    print(tsk)
     # Pour les 2 dernières conditions
     tasks = session.query(Tasks).filter_by(column = col_id).all()
+    print(tasks)
 
-    
-    # Changement de place juste a coter vers le bas
-    if(from_pos + 1 == to_pos):
-        old_position = task.position
-        task.position = tsk.position
-        tsk.position  = old_position
-    # Changement de placer juste a coter vers le haut
-    elif(from_pos - 1 == to_pos):
-        old_position = task.position
-        task.position = tsk.position
-        tsk.position  = old_position
+    if(task.column == col_id):        
+        # Changement de place juste a coter vers le bas
+        if(from_pos + 1 == to_pos):
+            old_position = task.position
+            task.position = tsk.position
+            tsk.position  = old_position
+        # Changement de placer juste a coter vers le haut
+        elif(from_pos - 1 == to_pos):
+            old_position = task.position
+            task.position = tsk.position
+            tsk.position  = old_position
 
-    # Changement de place sur plusieurs block vers le bas 
-    elif(from_pos < to_pos):
-        for tsk in tasks:
-            task.position= to_pos
-            if(tsk.id != task.id and tsk.position <= to_pos and tsk.position >= from_pos):
-                tsk.position -= 1
-    # Changement de place sur plusieurs block vers le haut
+        # Changement de place sur plusieurs block vers le bas 
+        elif(from_pos < to_pos):
+            for tsk in tasks:
+                task.position= to_pos
+                if(tsk.id != task.id and tsk.position <= to_pos and tsk.position >= from_pos):
+                    tsk.position -= 1
+        # Changement de place sur plusieurs block vers le haut
+        else:
+            for tsk in tasks:
+                task.position= to_pos
+                if(tsk.id != task.id and tsk.position >= to_pos and tsk.position <= from_pos):
+                    tsk.position += 1
     else:
-        for tsk in tasks:
-            task.position= to_pos
-            if(tsk.id != task.id and tsk.position >= to_pos and tsk.position <= from_pos):
-                tsk.position += 1
+        print('changement de colone')
+        task.column = col_id
+    session.commit()
+
+@hug.patch('')
+def updateTask(id, title):
+    task = session.query(Tasks).filter_by(id = id).first()
+    task.title = title
+    session.commit()
+    return 'ok'
+
+@hug.delete('')
+def delete(id):
+    print(id)
+    task = session.query(Tasks).filter_by(id = id).first()
+    session.delete(task)
     session.commit()
